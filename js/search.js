32392d481a9dd2596ee71c2e636c08ccf562d8ad
admin <133757594+eslionekim@users.noreply.github.com>
2025-04-19 01:30:23 +0900

    4주차 연습문제_자바스크립트 수정하기

--- js/search.js
+++ js/search.js
@@ -1,13 +1,36 @@
-document.getElementById("search_button_msg").addEventListener('click',search_message);
+document.getElementById("search_btn").addEventListener('click',search_message);
 function search_message(){
     alert("검색을 실시합니다!");
 }
 function search_message(){
     alert("입력하세요!");
 }
+
+const badWords=["시발","존나","개새끼","바보","멍청이"];
+
 function search_message(){
-    let search ="검색을 수행합니다"
-    alert(search);
+    let search=document.getElementById("search_input").value.trim();
+    if(search.length===0){
+        alert("공백입니다");
+        return false;
+    }
+
+    for(let word of badWords){
+        if(search.includes(word)){
+            alert("부적절한 검색어가 포함되어 있습니다");
+            return false;
+        }
+    }
+    alert("검색을 수행합니다."); // 공백 및 비속어 검사를 통과한 경우
+    return true; // 검색 수행 가능
 }
 //같은 이름의 함수가 여러개면 에러는 안나는데 가장 마지막 함수만 실행
 //let변수에 문자열 넣어서 출력하기
+
+function googleSearch() {
+    const searchTerm = document.getElementById("search_input").value; // 검색어로 설정
+    const googleSearchUrl = `https://www.google.com/search?q=${encodeURIComponent(searchTerm)}`;
+     // 새 창에서 구글 검색을 수행
+    window.open(googleSearchUrl, "_blank"); // 새로운 창에서 열기.
+     return false;
+     }
