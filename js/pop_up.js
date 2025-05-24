1ce9c9d40b0bc09b6f0e3e4ff74c3399cc39e2e1
admin <133757594+eslionekim@users.noreply.github.com>
2025-05-24 19:38:17 +0900

    7주차 수업내용_데이터저장(쿠키)

--- js/pop_up.js
+++ js/pop_up.js
@@ -1,5 +1,8 @@
-function pop_up(){ //이거 index.html에 추가
-    window.open("../popup/popup.html","팝업테스트","width=400,height=300,top=10,left=10");
+function pop_up(){ //index.html body태그에서 onload함, 메인페이지 치면 뜨는거거
+    var cookieCheck = getCookie("popupYN"); //getCookie(): 쿠키 값 읽기
+        if (cookieCheck != "N"){ //쿠키 값이 N이 아닐경우
+        window.open("../popup/popup.html", "팝업테스트", "width=400, height=300, top=10, left=10");
+    }
 }
 
 function show_clock(){
@@ -22,19 +25,45 @@ function show_clock(){
     }
     setTimeout(show_clock, 1000);  //1초마다갱신
    }
-   
-// function over(obj) {
-//     obj.src="image/face.png";
-// }
 
+//머무를 때 이미지
 const over=(obj)=>{
     obj.src="image/face.png";
 }
 
-// function out(obj) {
-//     obj.src="image/face_2.png";
-// }
-
+//나갔을 때 이미지
 const out=(obj)=>{
     obj.src="image/face_2.png";
+}
+
+function setCookie(name,value,expiredays){
+    var date=new Date(); //date: 현재 날짜 가져옴
+    date.setDate(date.getDate()+expiredays); 
+    //만료일=현재날짜(getDate로 받아옴)+지정날짜(expiredays)
+    document.cookie=escape(name)+"="+escape(value)+";expires="+date.toUTCString()+";path=/" + ";SameSite=None; Secure";
+    //쿠키설정=쿠키이름 URI인코딩 = 쿠키 값 URI인코딩 ;expires= 쿠키만료일+ 경로지정인데 /는 모든 경로에서 사용
+}
+
+function getCookie(name){ //name: 검색할 쿠키이름름
+    var cookie=document.cookie; //cookie: 모든 쿠키 문자열로 반환환
+    console.log("쿠키를 요청합니다.");
+    if (cookie!=""){ //쿠키가 뭐라도 있으면
+        var cookie_array=cookie.split("; "); //1=1 ; 2=2; -> [1=1,2=2]
+        //cookie에 저장된 쿠키들을 ; 기준으로 분리해서 배열로 변환
+        for (var index in cookie_array){ //쿠키 배열의 모든 것들을 순회
+            var cookie_name=cookie_array[index].split("="); //=를 기준으로 나누기, [1=1]->[1,1]
+            if (cookie_name[0]=="popupYN"){ //쿠키 이름이 popupYN
+                return cookie_name[1]; //해당 쿠키 값 반환 [0]이 이름, [1]이 값
+            }
+        }
+    }
+    return; //쿠키 없으면 결과 없음음
+}
+
+function closePopup(){
+    if(document.getElementById('check_popup').value){//id가 check_popup인 요소.체크박스 선택된 경우
+        setCookie("popupYN","N",1); //쿠키설정(이름,값,만료일1)
+        console.log("쿠키를 설정합니다.");
+        self.close();
+    }
 }
--- js/popup_close.js
+++ js/popup_close.js
@@ -1,9 +1,9 @@
 var close_time;
-var close_time2 = 10;
+var close_time2 = 50; //팝업창 유지 시간간
 
 clearTimeout(close_time); //이전에 설정된 타임아웃이 있으면 취소소
-close_time=setTimeout("close_window()",10000); //10초 후 close_window함수수
-show_time(); //카운드 다운 시작작
+close_time=setTimeout("close_window()",50000); //몇초 후 close_window함수
+show_time(); //카운드 다운 시작
 
 function show_time(){
     let divClock=document.getElementById('Time');
